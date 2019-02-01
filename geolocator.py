820e91651d96f7e1ed8155fda2ca276700b7631b
Tiju George <tiju.mathew@outlook.com>
2019-01-31 21:13:11 -0500

    Update geolocator.py

--- geolocator.py
+++ geolocator.py
@@ -7,3 +7,20 @@ x, y = nom.geocode(entry)
 
 print('Longitude =', y[0])
 print('Latitude =', y[1])
+
+
+'''
+import geopy
+
+from geopy.geocoders import Nominatim
+nom = Nominatim()
+entry = input("Enter your address: ")
+#x, y = nom.geocode(entry)
+#y = nom.geocode(entry)[1]
+if nom.geocode(entry) is None:
+	print("Not available")
+else:
+	print('Longitude =', nom.geocode(entry)[1][0])
+	print('Latitude =', nom.geocode(entry)[1][1])
+  
+  '''
